bcb2ddea9dfb5481b3f4029fc58be25499db1089
Junaid <onlyjunaid2002@gmail.com>
2024-06-16 08:02:49 +0530

    pdf gpt fixed uniqueness

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,13 +1,14 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { getPineconeClient } from "@/lib/pinecone";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from '@langchain/openai'
+import { PineconeStore } from '@langchain/pinecone'
+// import { getPineconeClient } from '@/lib/pinecone'
+import { Pinecone } from '@pinecone-database/pinecone'
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 
-const f = createUploadthing();
+const f = createUploadthing()
 const middleware = async () => {
   const { getUser } = getKindeServerSession()
   const user = getUser()
@@ -21,7 +22,7 @@ const middleware = async () => {
 
 const onUploadComplete = async ({
   metadata,
-  file,
+  file
 }: {
   metadata: Awaited<ReturnType<typeof middleware>>
   file: {
@@ -32,8 +33,8 @@ const onUploadComplete = async ({
 }) => {
   const isFileExist = await db.file.findFirst({
     where: {
-      key: file.key,
-    },
+      key: file.key
+    }
   })
 
   if (isFileExist) return
@@ -44,8 +45,8 @@ const onUploadComplete = async ({
       name: file.name,
       userId: metadata.userId,
       url: `https://utfs.io/f/${file.key}`,
-      uploadStatus: 'PROCESSING',
-    },
+      uploadStatus: 'PROCESSING'
+    }
   })
 
   try {
@@ -55,9 +56,9 @@ const onUploadComplete = async ({
     const pageLevelDocs = await loader.load()
     const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: 1000,
-      chunkOverlap: 200,
-    });
-    const chunkedDocs = await textSplitter.splitDocuments(pageLevelDocs);
+      chunkOverlap: 200
+    })
+    const chunkedDocs = await textSplitter.splitDocuments(pageLevelDocs)
 
     // pageLevelDocs.forEach((pageLevelDoc) => {
     //   console.log(pageLevelDoc)
@@ -67,67 +68,57 @@ const onUploadComplete = async ({
     //   }
     // }
     // )
+    pageLevelDocs.forEach((pageLevelDoc) => {
+      pageLevelDoc.metadata = {
+        ...pageLevelDoc.metadata,
+        fileId: createdFile.id,
+      };
+    });
     const pageAmt = pageLevelDocs.length
 
     //vectorize each page
-    const pinecone = await getPineconeClient()
+    const pinecone = new Pinecone({
+      apiKey: 'b33d6f38-01b1-4232-bc81-517d0318668d'
+      // environment: 'gcp-starter'
+    })
     const pineconeIndex = pinecone.Index('canopy--document-uploader')
-    console.log('pineconeIndex', pineconeIndex)
+    // console.log('pineconeIndex', pineconeIndex)
     const embeddings = new OpenAIEmbeddings({
-      openAIApiKey: process.env.OPENAI_API_KEY,
+      openAIApiKey: process.env.OPENAI_API_KEY
     })
 
-    // console.log(pageLevelDocs)
+    const pineconeStore = await PineconeStore.fromDocuments(pageLevelDocs,
+      embeddings, { pineconeIndex })
+    await pineconeStore.addDocuments(chunkedDocs)
 
-    // await PineconeStore.fromDocuments(
-    //   pageLevelDocs,
-    //   embeddings,
-    //   {
-    //     pineconeIndex,
-    //     maxConcurrency: 5,
-    //   }
-    // );
-    console.log(chunkedDocs)
-
-    await PineconeStore.fromDocuments(
-      chunkedDocs,
-      embeddings,
-      pineconeIndex
-    )
-
-
-
-    console.log('done vectorizing each page')
     await db.file.update({
       data: {
-        uploadStatus: 'SUCCESS',
+        uploadStatus: 'SUCCESS'
       },
       where: {
-        id: createdFile.id,
-      },
+        id: createdFile.id
+      }
     })
   } catch (error) {
     console.log(error)
     await db.file.update({
       where: {
-        id: createdFile.id,
+        id: createdFile.id
       },
       data: {
-        uploadStatus: 'FAILED',
-      },
+        uploadStatus: 'FAILED'
+      }
     })
     console.log(error)
-
   }
-
 }
 export const ourFileRouter = {
   freePlanUploader: f({ pdf: { maxFileSize: '4MB' } })
     .middleware(middleware)
-    .onUploadComplete(onUploadComplete),
+    .onUploadComplete(onUploadComplete)
   // proPlanUploader: f({ pdf: { maxFileSize: '16MB' } })
   //   .middleware(middleware)
   //   .onUploadComplete(onUploadComplete),
 } satisfies FileRouter
 
-export type OurFileRouter = typeof ourFileRouter;
+export type OurFileRouter = typeof ourFileRouter
